7451bcc1cd92a6c9df0a8f71f5376c80f312f5c9
McDuff, Calum J <cm19400@essex.ac.uk>
2020-11-06 11:04:56 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld, Xenios Christides");
         System.out.println("Hola, Xiaoyi sun here");
-        System.out.println("");
+        System.out.println("Hello World, Calum McDuff");
         System.out.println("");
         System.out.println("");
         System.out.println("");
