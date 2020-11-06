139e1e044d6bcce060ed8e042e987c23ccc2cbe9
calummcduff <calummcduff@gmail.com>
2020-11-06 11:12:44 +0000

    refactor of frame class to follow java conventions

---
+++ src/Team31/Frame.java
@@ -0,0 +1,16 @@
+package Team31;
+
+import javax.swing.JFrame;
+
+
+
+public class Frame extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI");
+        frame.setSize(200,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
