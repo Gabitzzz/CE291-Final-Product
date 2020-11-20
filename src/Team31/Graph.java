1192655faf94dc1db14df4f3d91fb794e4287f0e
Calum McDuff <cm19400@essex.ac.uk>
2020-11-20 11:27:44 +0000

    Revert Removal of Graph.java

---
+++ src/Team31/Graph.java
@@ -0,0 +1,178 @@
+package Team31;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class Graph extends JPanel {
+
+    private static ArrayList<Cases> casesArray;
+    private int width = 800;
+    private int heigth = 400;
+    private int padding = 25;
+    private int labelPadding = 25;
+    private Color lineColor = new Color(44, 102, 230, 180);
+    private Color pointColor = new Color(100, 100, 100, 180);
+    private Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private int pointWidth = 4;
+    private int numberYDivisions = 10;
+
+    public Graph(ArrayList<Cases> casesArray) {
+        this.casesArray = casesArray;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double xScale = ((int) getWidth() - (2 * padding) - labelPadding) / (casesArray.size() - 1);
+        double yScale = ((int) getHeight() - 2 * padding - labelPadding) / (getMaxCase() - getMinCase());
+
+        List<Point> graphPoints = new ArrayList<>();
+        for (int i = 0; i < casesArray.size(); i++) {
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxCase() - casesArray.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (casesArray.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinCase() + (getMaxCase() - getMinCase()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        for (int i = 0; i < casesArray.size(); i++) {
+            if (casesArray.size() > 1) {
+                int x0 = i * (getWidth() - padding * 2 - labelPadding) / (casesArray.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((casesArray.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+                }
+                g2.drawLine(x0, y0, x1, y1);
+            }
+        }
+
+        // create x and y axes
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+
+        Stroke oldStroke = g2.getStroke();
+        g2.setColor(lineColor);
+        g2.setStroke(GRAPH_STROKE);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
+        }
+
+        g2.setStroke(oldStroke);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size(); i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            int ovalW = pointWidth;
+            int ovalH = pointWidth;
+            g2.fillOval(x, y, ovalW, ovalH);
+        }
+    }
+
+    //    @Override
+//    public Dimension getPreferredSize() {
+//        return new Dimension(width, heigth);
+//    }
+    private int getMinCase() {
+        int minCase = int.MAX_VALUE;
+        for (int case : casesArray) {
+            minCase = Math.min(minCase, case);
+        }
+        return minCase;
+    }
+
+    private double getMaxCase() {
+        int maxCase = (int) MIN_VALUE;
+        for (Integer case : casesArray) {
+            maxCase = Math.max(maxCase, case);
+        }
+        return maxCase;
+    }
+
+    public void setCases(ArrayList<Cases> setCases) {
+        this.casesArray = cases;
+        invalidate();
+        this.repaint();
+    }
+
+    public ArrayList<Cases> getCases() {
+        return casesArray;
+    }
+
+    private static void createAndShowGui() {
+        ArrayList<Cases> casesArray = new ArrayList<>();
+        Random random = new Random();
+        int maxDataPoints = 40;
+        int maxScore = 10;
+        for (int i = 0; i < maxDataPoints; i++) {
+            casesArray.add((case) random.nextInt() * maxCase);
+
+        }
+        Graph mainPanel = new Graph(casesArray);
+        mainPanel.setPreferredSize(new Dimension(800, 600));
+        JFrame frame = new JFrame("DrawGraph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+
+    }
+/* This is to be transferred to main class
+        SwingUtilities.invokeLater(new Runnable() {
+public void run() {
+        createAndShowGui();
+        }
+        });
