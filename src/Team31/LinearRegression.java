bb8d5dfab778300fb679275546814e61b97b34bc
Xenios <xc19359@essex.ac.uk>
2020-12-01 20:00:16 +0200

    LinearRegression started!

---
+++ src/Team31/LinearRegression.java
@@ -0,0 +1,12 @@
+package Team31;
+
+import java.util.ArrayList;
+
+public class LinearRegression {
+
+    private ArrayList<Cases> cases ;
+    private ArrayList<Deaths> deaths;
+    private Float result1;
+    private Float result2;
+
+}
