5609288234e65a608fd15b73c626db113bebe5c8
Ferhat105 <73355416+Ferhat105@users.noreply.github.com>
2021-03-09 16:03:49 +0000

    fixed minor jar issues

--- src/Team31/Data.java
+++ src/Team31/Data.java
@@ -6,6 +6,7 @@ import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
@@ -26,56 +27,67 @@ public class Data {
     }
 
     public void readFile(int file) { // reads the specified file (cases or deaths)
-        String filePath;
+       try {
+           String filePath;
+           BufferedReader br;
 
-        if (file == Config.DEATHS_FILE) {
-            filePath = Config.DEATHS_FILEPATH;
+           if (file == Config.DEATHS_FILE) {
+               filePath = Config.DEATHS_FILEPATH;
+               URL url = getClass().getResource(filePath);
+               InputStream is = url.openStream();
+               br = new BufferedReader(new InputStreamReader(is));
 
-            deathsArray = new ArrayList<>(); // ensures the array is empty before manipulation
+               deathsArray = new ArrayList<>(); // ensures the array is empty before manipulation
 
-        } else if (file == Config.CASES_FILE) {
-            filePath = Config.CASES_FILEPATH;
+           } else if (file == Config.CASES_FILE) {
+               filePath = Config.CASES_FILEPATH;
+               URL url = getClass().getResource(filePath);
+               InputStream is = url.openStream();
+               br = new BufferedReader(new InputStreamReader(is));
 
-            casesArray = new ArrayList<>(); // ensures the array is empty before manipulation
-        } else if (file == Config.OTHER_FILE) {
-            filePath = path.toString();
+               casesArray = new ArrayList<>(); // ensures the array is empty before manipulation
+           } else if (file == Config.OTHER_FILE) {
+               filePath = path.toString();
+               br = new BufferedReader(new FileReader(filePath));
 
-            otherArray = new ArrayList<>(); // ensures the array is empty before manipulation
-        } else {
-            return;
-        }
+               otherArray = new ArrayList<>(); // ensures the array is empty before manipulation
+           } else {
+               return;
+           }
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            boolean firstLine = true;
+           try {
+               String line;
+               boolean firstLine = true;
 
-            while ((line = br.readLine()) != null) { // loops through file line by line whilst there is still a non null line
-                if (!firstLine) { // ensures that we dont attempt to pass to the array the first line of the csv
-                    String[] values = line.split(Config.FILE_DELIMITER); // splits the lines by the provided delimiter
+               while ((line = br.readLine()) != null) { // loops through file line by line whilst there is still a non null line
+                   if (!firstLine) { // ensures that we dont attempt to pass to the array the first line of the csv
+                       String[] values = line.split(Config.FILE_DELIMITER); // splits the lines by the provided delimiter
 
-                    if (file == Config.DEATHS_FILE) {
-                        deathsArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5])));// adds to the deathsArray a reference to the new Deaths Object
-                    }
-                    else if(file == Config.CASES_FILE) {
-                        if (values.length < 6) { // checks to see if the values array is less that 6 (some data was missing in the "cumCasesByPublishDate" column in the csv)
-                            values = new String[] {values[0], values[1], values[2], values[3], values[4], "0"}; // if data is missing, assigns a 0
-                        }
+                       if (file == Config.DEATHS_FILE) {
+                           deathsArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5])));// adds to the deathsArray a reference to the new Deaths Object
+                       } else if (file == Config.CASES_FILE) {
+                           if (values.length < 6) { // checks to see if the values array is less that 6 (some data was missing in the "cumCasesByPublishDate" column in the csv)
+                               values = new String[]{values[0], values[1], values[2], values[3], values[4], "0"}; // if data is missing, assigns a 0
+                           }
 
-                        casesArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5]))); // adds to the casesArray a reference to the new Cases Object
-                    } else {
-                        if (values.length < 6) { // checks to see if the values array is less that 6 (some data was missing in the "cumCasesByPublishDate" column in the csv)
-                            values = new String[] {values[0], values[1], values[2], values[3], values[4], "0"}; // if data is missing, assigns a 0
-                        }
+                           casesArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5]))); // adds to the casesArray a reference to the new Cases Object
+                       } else {
+                           if (values.length < 6) { // checks to see if the values array is less that 6 (some data was missing in the "cumCasesByPublishDate" column in the csv)
+                               values = new String[]{values[0], values[1], values[2], values[3], values[4], "0"}; // if data is missing, assigns a 0
+                           }
 
-                        otherArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5]))); // adds to the casesArray a reference to the new Cases Object
-                    }
-                } else {
-                    firstLine = false; // sets variable to false after the first loop, therefore skipping first line
-                }
-            }
-        } catch (IOException | IndexOutOfBoundsException e) { // catches exception so program don't crash
-            JOptionPane.showMessageDialog(null,"Selected file may be deleted or removed from the original path.","File Not Found", JOptionPane.ERROR_MESSAGE);
-        }
+                           otherArray.add(new DataStore(values[3], Long.parseLong(values[4]), Long.parseLong(values[5]))); // adds to the casesArray a reference to the new Cases Object
+                       }
+                   } else {
+                       firstLine = false; // sets variable to false after the first loop, therefore skipping first line
+                   }
+               }
+           } catch (IOException | IndexOutOfBoundsException e) { // catches exception so program don't crash
+               JOptionPane.showMessageDialog(null, "Selected file may be deleted or removed from the original path.", "File Not Found", JOptionPane.ERROR_MESSAGE);
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
     }
 
     public void saveAsImage(JFrame frame)
